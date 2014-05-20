3f392a36211e990ca9a6ab6ffd6abcc157a155c8
PeterWAWood <PeterWAWood@gmail.com>
2014-05-20 10:57:58 +0800

    DRYed out Java tests

--- Java/UnicodeTests.java
+++ Java/UnicodeTests.java
@@ -14,6 +14,15 @@ public class UnicodeTests {
     failed += 1;
     System.out.println("Test " + testName + " failed");
   }
+  
+  static void test(String name, boolean result) {
+    if (result) {
+      passed++;
+    } else {
+      failed++;
+      System.out.println("Test " + name + " failed");
+    }
+  }
 
   static int countGraphemes(String input) {
     BreakIterator graphemes = BreakIterator.getCharacterInstance();
@@ -49,133 +58,48 @@ public class UnicodeTests {
     Locale turkish = new Locale("tr", "TR");
     Locale german = new Locale("de", "DE");
     
-    testName = "Unicode 1";
-    unicodeString = "c\u0327";
-    if (Normalizer.normalize(unicodeString, Normalizer.Form.NFC).equals("\u00E7")) {
-      passed += 1;
-    } else {
-      testFailed();
-    }
+    test("Unicode 1", Normalizer.normalize("c\u0327", Normalizer.Form.NFC).equals("\u00E7"));
     
-    testName = "Unicode 2";
-    unicodeString = "c\u0327";
-    if (!unicodeString.equals("\u00E7")) {
-      passed += 1;
-    } else {
-      testFailed();
-    }
+    test("Unicode 2", !"c\u0327".equals("\u00E7"));
     
-    testName = "Unicode 3";
-    if (countGraphemes("noe\u0308l") == 4) {
-      passed += 1;
-    } else {
-      testFailed();
-    }
+    test("Unicode 3", countGraphemes("noe\u0308l") == 4);
     
-    testName = "Unicode 4";
-    if (reverseGraphemes("noe\u0308l").equals("le\u0308on")) {
-      passed += 1;
-    } else {
-      testFailed();
-    }
+    test("Unicode 4", reverseGraphemes("noe\u0308l").equals("le\u0308on"));
     
-    testName = "Unicode 5";
-    unicodeString = "noe\u0308l";
-    if (substringGraphemes(unicodeString, 0, 3).equals("noe\u0308")
-            && countGraphemes(substringGraphemes(unicodeString, 0, 3)) == 3) {
-      passed += 1;
-    } else {
-      testFailed();
-    }
+    test("Unicode 5", substringGraphemes("noe\u0308l", 0, 3).equals("noe\u0308")
+                      && countGraphemes(substringGraphemes("noe\u0308l", 0, 3)) == 3);
+            
+    test("Unicode 6", "ba\uFB04e".toUpperCase().equals("BAFFLE"));
     
-    testName = "Unicode 6";
-    unicodeString = "ba\uFB04e";
-    if (unicodeString.toUpperCase().equals("BAFFLE")) {
-      passed += 1;
-    } else {
-      testFailed();
-    }
-  
-    testName = "Unicode 7";
-    unicodeString = "cant\u00F9";
-    if (unicodeString.toUpperCase().equals("CANT\u00D9")) {
-      passed += 1;
-    } else {
-      testFailed();
-    }
+    test("Unicode 7", "cant\u00F9".toUpperCase().equals("CANT\u00D9"));
     
-    testName = "Unicode 8";
-    unicodeString = "cantu\u0300";
-    if (unicodeString.toUpperCase().equals("CANTU\u0300")) {
-      passed += 1;
-    } else {
-      testFailed();
-    }
+    test("Unicode 8", "cantu\u0300".toUpperCase().equals("CANTU\u0300"));
     
-    testName = "Unicode 9";
     unicodeString = "\uD834\uDD1E - The Treble Clef";
     unicodeString = unicodeString.replace("\uD834\uDD1E", "\uD834\uDD22");
-    if (unicodeString.replace("Treble", "Bass").equals("\uD834\uDD22 - The Bass Clef")) {
-      passed += 1;
-    } else {
-      testFailed();
-    }   
+    test("Unicode 9", unicodeString.replace("Treble", "Bass")
+                                    .equals("\uD834\uDD22 - The Bass Clef"));
     
-    testName = "Unicode 10";
-    unicodeString = "\uD834\uDD22 - The Bass Clef";
-    if (unicodeString.codePointCount(0, unicodeString.length()) == 17 ) { 
-      passed += 1;
-    } else {
-      testFailed();
-    }   
+    test("Unicode 10", "\uD834\uDD22 - The Bass Clef".codePointCount(
+                        0, "\uD834\uDD22 - The Bass Clef".length()) == 17 );
+                                                      
     
-    testName = "Unicode 11";
-    unicodeString = "i";
-    if (unicodeString.toUpperCase(turkish).equals("\u0130")) {
-      passed += 1;
-    } else {
-      testFailed();
-    }
+    test("Unicode 11", "i".toUpperCase(turkish).equals("\u0130"));
     
-    testName = "Unicode 12";
-    unicodeString = "I";
-    if (unicodeString.toLowerCase(turkish).equals("\u0131")) {
-      passed += 1;
-    } else {
-      testFailed();
-    }
+    test("Unicode 12", "I".toLowerCase(turkish).equals("\u0131"));
     
-    testName = "Unicode 13";
-    unicodeString = "stra\u00DFe";
-    if (unicodeString.toUpperCase().equals("STRASSE")) {
-      passed += 1;
-    } else {
-      testFailed();
-    }
+    test("Unicode 13", "stra\u00DFe".toUpperCase().equals("STRASSE"));
     
-    testName = "Unicode 14";
-    unicodeString = "\u03C8\u3099";
-    if (countGraphemes(Normalizer.normalize(unicodeString, Normalizer.Form.NFC)) == 1) {
-      passed += 1;
-    } else {
-      testFailed();
-    }
+    test("Unicode 14", countGraphemes(Normalizer.normalize("\u03C8\u3099", 
+                                      Normalizer.Form.NFC)) == 1);
     
-    testName = "Unicode 15";
-    unicodeString = "e\u0308\uD834\uDD1E\u03C8\u3099";
-    if (countGraphemes(Normalizer.normalize(unicodeString, Normalizer.Form.NFC)) == 3) {
-      passed += 1;
-    } else {
-      testFailed();
-    }
+    test("Unicode 15", countGraphemes(
+                          Normalizer.normalize("e\u0308\uD834\uDD1E\u03C8\u3099", 
+                          Normalizer.Form.NFC)) == 3); 
     
-    testName = "Unicode 16";
+
     Collator deCollator = Collator.getInstance(german);
-    if (deCollator.compare("weiss", "weiß") == 0) {
-      passed += 1;
-    } else {
-      testFailed();
-    }
+    test("Unicode 16", deCollator.compare("weiss", "weiß") == 0);
     
     System.out.println("Tests performed: " + (passed + failed));
     System.out.println("Tests passed:    " + passed);
